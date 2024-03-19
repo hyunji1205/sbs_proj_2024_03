2ffcd57d820b09c64f01f448fc6f1fda4d1dc72b
hyunji1205 <sguswl1205@gmail.com>
2024-03-19 18:52:36 +0900

    스캐너 입력받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 프로젝트 시작 ==");
+        Scanner sc = new Scanner(System.in);
+        // 키보드를 스캔한다
+
+        System.out.printf("명령어) ");
+        String cmd = sc.nextLine();
+        // sc.nextLine() : 한 라인을 의미 (엔터치기 전까지)
+        // sc.next : 띄어쓰기 전까지
+        // 사용자의 키보드를 받는다
+        // 여기서 멈춤
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+        System.out.printf("명령어) ");
+        int num = sc.nextInt();
+        System.out.printf("입력된 명령어 : %d\n", num);
+
+
+
+        sc.close();
+        // 닫기 버튼
+        System.out.println("== 프로젝트 끝 ==");
     }
 }
