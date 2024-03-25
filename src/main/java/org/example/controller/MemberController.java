08a232bbec663ab4e3af3c49d27668ecd21dfa96
hyunji1205 <sguswl1205@gmail.com>
2024-03-25 17:08:31 +0900

    한 번 사용된 게시물 번호가 재사용되지 않도록 lastId 도입2

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -26,8 +26,8 @@ public class MemberController extends Controller {
         System.out.println("테스트를 위한 회원 데이터를 생성합니다");
 
         Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "admin", "admin", "송현지"));
-        Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user", "user", "황예지"));
-        Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user1", "user1", "신유나"));
+        Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user", "user", "박재민"));
+        Container.memberDao.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "user1", "user1", "송인규"));
     }
     public void doAction(String cmd, String actionMethodName){
         this.cmd = cmd;
